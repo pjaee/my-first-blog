5313ef1e1400b3394dd6cd73972205781a148e80
Pjaee <j1beatz@naver.com>
2019-02-20 23:21:43 +0900

    real first commit

--- j1beatz/settings.py
+++ j1beatz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7kxq%2j$wc_k6$m@7oi@^lx*^d(pu-yru+!#cea87_iu98-1#e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
